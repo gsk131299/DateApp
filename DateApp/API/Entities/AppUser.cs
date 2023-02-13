bfcb98e3fe81af7ad2a5253508cccab17a09e75d
gsk131299 <gsk131299@gmail.com>
2023-02-13 18:17:19 +0530

    end of section 2

---
+++ DateApp/API/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+ 
+        public String UserName { get; set; }
+
+        
+    }
+}
